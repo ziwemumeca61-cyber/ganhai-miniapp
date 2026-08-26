1bc35234f994d10aa07b88837803dfd6c1434a9c
WaveQuester <ziwemumeca61@gmail.com>
2026-08-26 10:39:30 +0800

    启用赶海雷达云开发环境

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ App({
 
   onLaunch() {
     // 配置自己的 CloudBase 环境 ID 后，再打开云开发和成长计划 AI。
-    const cloudEnv = ''
+    const cloudEnv = 'ganhai-miniapp-d7g9754k00dfaa66b'
     if (wx.cloud && cloudEnv) {
       wx.cloud.init({
         env: cloudEnv,
