9ebd9097c27ee4dd6c832ff9d64a660eafbae1a5
WaveQuester <ziwemumeca61@gmail.com>
2026-08-29 10:05:23 +0800

    feat: add nationwide report spot whitelist

---
+++ cloudfunctions/report/national-spots.js
@@ -0,0 +1,548 @@
+// 全国沿海地点云端白名单；附近坐标不代表精确入口。
+module.exports = {
+  "dalian-fujiazhuang": {
+    "name": "傅家庄海滨浴场（附近）",
+    "latitude": 38.866,
+    "longitude": 121.614
+  },
+  "dalian-xiajiahezi": {
+    "name": "夏家河子海滨公园（附近）",
+    "latitude": 39.009,
+    "longitude": 121.546
+  },
+  "dalian-jinshitan": {
+    "name": "金石滩黄金海岸（附近）",
+    "latitude": 39.083,
+    "longitude": 122.008
+  },
+  "dandong-zhangdao": {
+    "name": "獐岛海滨岸段（附近）",
+    "latitude": 39.759,
+    "longitude": 123.596
+  },
+  "dandong-daludao": {
+    "name": "大鹿岛海滨浴场（附近）",
+    "latitude": 39.752,
+    "longitude": 123.745
+  },
+  "yingkou-shanhai": {
+    "name": "山海广场海滩（附近）",
+    "latitude": 40.219,
+    "longitude": 122.118
+  },
+  "yingkou-baishawan": {
+    "name": "白沙湾黄金海岸（附近）",
+    "latitude": 40.006,
+    "longitude": 122.108
+  },
+  "panjin-redbeach": {
+    "name": "红海滩国家风景廊道（附近）",
+    "latitude": 40.893,
+    "longitude": 121.999
+  },
+  "panjin-liaodongbay": {
+    "name": "辽东湾新区滨海岸段（附近）",
+    "latitude": 40.735,
+    "longitude": 121.935
+  },
+  "jinzhou-bijiashan": {
+    "name": "笔架山海滨（附近）",
+    "latitude": 40.823,
+    "longitude": 121.073
+  },
+  "jinzhou-bay": {
+    "name": "锦州湾海滨浴场（附近）",
+    "latitude": 40.806,
+    "longitude": 121.132
+  },
+  "huludao-xingcheng": {
+    "name": "兴城海滨浴场（附近）",
+    "latitude": 40.616,
+    "longitude": 120.754
+  },
+  "huludao-longhuitou": {
+    "name": "龙回头海滨岸段（附近）",
+    "latitude": 40.65,
+    "longitude": 120.839
+  },
+  "qinhuangdao-geziwo": {
+    "name": "鸽子窝公园海岸（附近）",
+    "latitude": 39.825,
+    "longitude": 119.531
+  },
+  "qinhuangdao-nandaihe": {
+    "name": "南戴河海滨浴场（附近）",
+    "latitude": 39.785,
+    "longitude": 119.432
+  },
+  "tangshan-xiangyun": {
+    "name": "祥云湾海滨温泉度假区（附近）",
+    "latitude": 39.185,
+    "longitude": 118.981
+  },
+  "tangshan-putidao": {
+    "name": "菩提岛湿地岸段（附近）",
+    "latitude": 39.116,
+    "longitude": 118.851
+  },
+  "cangzhou-nandagang": {
+    "name": "南大港湿地（附近）",
+    "latitude": 38.502,
+    "longitude": 117.527
+  },
+  "cangzhou-bohai": {
+    "name": "渤海新区滨海岸段（附近）",
+    "latitude": 38.32,
+    "longitude": 117.891
+  },
+  "tianjin-dongjiang": {
+    "name": "东疆湾沙滩景区（附近）",
+    "latitude": 39,
+    "longitude": 117.802
+  },
+  "tianjin-maritime": {
+    "name": "国家海洋博物馆滨海岸段（附近）",
+    "latitude": 38.924,
+    "longitude": 117.757
+  },
+  "lianyungang-liandao": {
+    "name": "连岛大沙湾（附近）",
+    "latitude": 34.756,
+    "longitude": 119.468
+  },
+  "lianyungang-zaihai": {
+    "name": "在海一方公园岸段（附近）",
+    "latitude": 34.751,
+    "longitude": 119.393
+  },
+  "yancheng-tiaozini": {
+    "name": "条子泥湿地（附近）",
+    "latitude": 32.758,
+    "longitude": 120.903
+  },
+  "yancheng-sheyang": {
+    "name": "射阳河口滨海岸段（附近）",
+    "latitude": 33.839,
+    "longitude": 120.493
+  },
+  "nantong-golden": {
+    "name": "启东黄金海滩（附近）",
+    "latitude": 31.709,
+    "longitude": 121.984
+  },
+  "nantong-yuantuojiao": {
+    "name": "圆陀角滨海岸段（附近）",
+    "latitude": 31.728,
+    "longitude": 121.899
+  },
+  "shanghai-fengxian": {
+    "name": "碧海金沙水上乐园（附近）",
+    "latitude": 30.82,
+    "longitude": 121.518
+  },
+  "shanghai-jinshan": {
+    "name": "金山城市沙滩（附近）",
+    "latitude": 30.724,
+    "longitude": 121.35
+  },
+  "shanghai-nanhui": {
+    "name": "南汇嘴观海公园（附近）",
+    "latitude": 30.866,
+    "longitude": 121.947
+  },
+  "jiaxing-jiulongshan": {
+    "name": "九龙山滨海岸段（附近）",
+    "latitude": 30.591,
+    "longitude": 121.122
+  },
+  "jiaxing-guanhaiyuan": {
+    "name": "海盐观海园岸段（附近）",
+    "latitude": 30.542,
+    "longitude": 120.998
+  },
+  "ningbo-songlanshan": {
+    "name": "松兰山海滨浴场（附近）",
+    "latitude": 29.509,
+    "longitude": 121.919
+  },
+  "ningbo-banbianshan": {
+    "name": "半边山沙滩（附近）",
+    "latitude": 29.186,
+    "longitude": 121.95
+  },
+  "zhoushan-nansha": {
+    "name": "朱家尖南沙（附近）",
+    "latitude": 29.882,
+    "longitude": 122.389
+  },
+  "zhoushan-dongsha": {
+    "name": "朱家尖东沙（附近）",
+    "latitude": 29.899,
+    "longitude": 122.396
+  },
+  "zhoushan-jihu": {
+    "name": "嵊泗基湖沙滩（附近）",
+    "latitude": 30.727,
+    "longitude": 122.45
+  },
+  "taizhou-dongxia": {
+    "name": "洞下沙滩（附近）",
+    "latitude": 28.383,
+    "longitude": 121.619
+  },
+  "taizhou-dachen": {
+    "name": "大陈岛海滨岸段（附近）",
+    "latitude": 28.451,
+    "longitude": 121.899
+  },
+  "wenzhou-banping": {
+    "name": "洞头半屏山海岸（附近）",
+    "latitude": 27.84,
+    "longitude": 121.171
+  },
+  "wenzhou-nanji": {
+    "name": "南麂岛大沙岙（附近）",
+    "latitude": 27.46,
+    "longitude": 121.052
+  },
+  "ningde-beiqi": {
+    "name": "霞浦北岐滩涂（附近）",
+    "latitude": 26.885,
+    "longitude": 120.018
+  },
+  "ningde-dongbi": {
+    "name": "霞浦东壁海岸（附近）",
+    "latitude": 26.884,
+    "longitude": 120.139
+  },
+  "fuzhou-longwangtou": {
+    "name": "平潭龙王头沙滩（附近）",
+    "latitude": 25.504,
+    "longitude": 119.799
+  },
+  "fuzhou-xiasha": {
+    "name": "长乐下沙海滩（附近）",
+    "latitude": 25.879,
+    "longitude": 119.665
+  },
+  "putian-meizhou": {
+    "name": "湄洲岛黄金沙滩（附近）",
+    "latitude": 25.083,
+    "longitude": 119.128
+  },
+  "putian-rinan": {
+    "name": "南日岛海滨岸段（附近）",
+    "latitude": 25.203,
+    "longitude": 119.228
+  },
+  "quanzhou-chongwu": {
+    "name": "崇武古城海岸（附近）",
+    "latitude": 24.888,
+    "longitude": 118.921
+  },
+  "quanzhou-goldcoast": {
+    "name": "石狮黄金海岸（附近）",
+    "latitude": 24.73,
+    "longitude": 118.688
+  },
+  "xiamen-huangcuo": {
+    "name": "黄厝海滩（附近）",
+    "latitude": 24.438,
+    "longitude": 118.151
+  },
+  "xiamen-wuyuan": {
+    "name": "五缘湾沙滩（附近）",
+    "latitude": 24.532,
+    "longitude": 118.171
+  },
+  "zhangzhou-maluan": {
+    "name": "东山马銮湾（附近）",
+    "latitude": 23.719,
+    "longitude": 117.52
+  },
+  "zhangzhou-liuao": {
+    "name": "六鳌翡翠湾（附近）",
+    "latitude": 23.978,
+    "longitude": 117.744
+  },
+  "chaozhou-greenisland": {
+    "name": "饶平绿岛旅游山庄海岸（附近）",
+    "latitude": 23.617,
+    "longitude": 117.102
+  },
+  "chaozhou-xunzhou": {
+    "name": "汛洲岛海滨岸段（附近）",
+    "latitude": 23.6,
+    "longitude": 116.995
+  },
+  "shantou-qingao": {
+    "name": "南澳青澳湾（附近）",
+    "latitude": 23.429,
+    "longitude": 117.133
+  },
+  "shantou-zhongxin": {
+    "name": "中信滨海沙滩（附近）",
+    "latitude": 23.268,
+    "longitude": 116.751
+  },
+  "jieyang-huilai": {
+    "name": "惠来滨海旅游度假区（附近）",
+    "latitude": 22.988,
+    "longitude": 116.31
+  },
+  "jieyang-keweiyi": {
+    "name": "客鸟尾石笋岸段（附近）",
+    "latitude": 22.953,
+    "longitude": 116.491
+  },
+  "shanwei-redbay": {
+    "name": "红海湾遮浪海滩（附近）",
+    "latitude": 22.668,
+    "longitude": 115.562
+  },
+  "shanwei-jinding": {
+    "name": "金町湾沙滩（附近）",
+    "latitude": 22.792,
+    "longitude": 115.305
+  },
+  "huizhou-shuangyue": {
+    "name": "双月湾沙滩（附近）",
+    "latitude": 22.563,
+    "longitude": 114.894
+  },
+  "huizhou-xunliao": {
+    "name": "巽寮湾沙滩（附近）",
+    "latitude": 22.7,
+    "longitude": 114.739
+  },
+  "shenzhen-dameisha": {
+    "name": "大梅沙海滨公园（附近）",
+    "latitude": 22.596,
+    "longitude": 114.306
+  },
+  "shenzhen-xichong": {
+    "name": "西涌海滩（附近）",
+    "latitude": 22.481,
+    "longitude": 114.545
+  },
+  "dongguan-weiyuan": {
+    "name": "威远岛滨海岸段（附近）",
+    "latitude": 22.807,
+    "longitude": 113.619
+  },
+  "dongguan-shajiao": {
+    "name": "沙角炮台滨江岸段（附近）",
+    "latitude": 22.755,
+    "longitude": 113.67
+  },
+  "guangzhou-nansha": {
+    "name": "南沙天后宫滨海岸段（附近）",
+    "latitude": 22.755,
+    "longitude": 113.61
+  },
+  "guangzhou-wetland": {
+    "name": "南沙湿地公园（附近）",
+    "latitude": 22.618,
+    "longitude": 113.632
+  },
+  "zhongshan-yakou": {
+    "name": "崖口滨海岸段（附近）",
+    "latitude": 22.499,
+    "longitude": 113.573
+  },
+  "zhongshan-cuiheng": {
+    "name": "翠亨湿地岸段（附近）",
+    "latitude": 22.449,
+    "longitude": 113.557
+  },
+  "zhuhai-xianglu": {
+    "name": "香炉湾沙滩（附近）",
+    "latitude": 22.27,
+    "longitude": 113.591
+  },
+  "zhuhai-dongao": {
+    "name": "东澳岛南沙湾（附近）",
+    "latitude": 22.017,
+    "longitude": 113.709
+  },
+  "jiangmen-naqin": {
+    "name": "那琴半岛海岸（附近）",
+    "latitude": 21.817,
+    "longitude": 112.581
+  },
+  "jiangmen-wangfuzhou": {
+    "name": "下川岛王府洲（附近）",
+    "latitude": 21.617,
+    "longitude": 112.574
+  },
+  "yangjiang-dajiaowan": {
+    "name": "海陵岛大角湾（附近）",
+    "latitude": 21.572,
+    "longitude": 111.852
+  },
+  "yangjiang-shili": {
+    "name": "十里银滩（附近）",
+    "latitude": 21.603,
+    "longitude": 111.91
+  },
+  "maoming-firstbeach": {
+    "name": "中国第一滩（附近）",
+    "latitude": 21.468,
+    "longitude": 111.043
+  },
+  "maoming-fangji": {
+    "name": "放鸡岛海岸（附近）",
+    "latitude": 21.39,
+    "longitude": 111.213
+  },
+  "zhanjiang-jinshawan": {
+    "name": "金沙湾海滨浴场（附近）",
+    "latitude": 21.272,
+    "longitude": 110.398
+  },
+  "zhanjiang-techeng": {
+    "name": "特呈岛海滨岸段（附近）",
+    "latitude": 21.151,
+    "longitude": 110.438
+  },
+  "beihai-silver": {
+    "name": "北海银滩（附近）",
+    "latitude": 21.405,
+    "longitude": 109.173
+  },
+  "beihai-qiaogang": {
+    "name": "侨港海滩（附近）",
+    "latitude": 21.421,
+    "longitude": 109.119
+  },
+  "qinzhou-sanniang": {
+    "name": "三娘湾海滩（附近）",
+    "latitude": 21.608,
+    "longitude": 108.755
+  },
+  "qinzhou-maoweihai": {
+    "name": "茅尾海湿地岸段（附近）",
+    "latitude": 21.829,
+    "longitude": 108.574
+  },
+  "fangcheng-bailang": {
+    "name": "白浪滩（附近）",
+    "latitude": 21.584,
+    "longitude": 108.222
+  },
+  "fangcheng-wanwei": {
+    "name": "万尾金滩（附近）",
+    "latitude": 21.552,
+    "longitude": 108.034
+  },
+  "haikou-holiday": {
+    "name": "假日海滩（附近）",
+    "latitude": 20.041,
+    "longitude": 110.198
+  },
+  "haikou-xixiu": {
+    "name": "西秀海滩（附近）",
+    "latitude": 20.024,
+    "longitude": 110.243
+  },
+  "wenchang-moonbay": {
+    "name": "月亮湾海滩（附近）",
+    "latitude": 19.65,
+    "longitude": 111.036
+  },
+  "wenchang-gaolong": {
+    "name": "高隆湾海滩（附近）",
+    "latitude": 19.54,
+    "longitude": 110.82
+  },
+  "qionghai-boao": {
+    "name": "博鳌海滨岸段（附近）",
+    "latitude": 19.159,
+    "longitude": 110.582
+  },
+  "qionghai-tanmen": {
+    "name": "潭门港外侧海滩（附近）",
+    "latitude": 19.241,
+    "longitude": 110.687
+  },
+  "wanning-riyue": {
+    "name": "日月湾海滩（附近）",
+    "latitude": 18.681,
+    "longitude": 110.274
+  },
+  "wanning-shimei": {
+    "name": "石梅湾海滩（附近）",
+    "latitude": 18.662,
+    "longitude": 110.29
+  },
+  "lingshui-qingshui": {
+    "name": "清水湾沙滩（附近）",
+    "latitude": 18.404,
+    "longitude": 109.96
+  },
+  "lingshui-xiangshui": {
+    "name": "香水湾海滩（附近）",
+    "latitude": 18.459,
+    "longitude": 110.035
+  },
+  "sanya-yalong": {
+    "name": "亚龙湾海滩（附近）",
+    "latitude": 18.23,
+    "longitude": 109.641
+  },
+  "sanya-dadonghai": {
+    "name": "大东海海滩（附近）",
+    "latitude": 18.22,
+    "longitude": 109.522
+  },
+  "ledong-longmu": {
+    "name": "龙沐湾海滩（附近）",
+    "latitude": 18.69,
+    "longitude": 108.731
+  },
+  "ledong-yinggehai": {
+    "name": "莺歌海海滨岸段（附近）",
+    "latitude": 18.509,
+    "longitude": 108.704
+  },
+  "dongfang-yulinzhou": {
+    "name": "鱼鳞洲海滩（附近）",
+    "latitude": 19.102,
+    "longitude": 108.621
+  },
+  "dongfang-haidongfang": {
+    "name": "海东方沙滩（附近）",
+    "latitude": 19,
+    "longitude": 108.65
+  },
+  "danzhou-haihua": {
+    "name": "海花岛海滨岸段（附近）",
+    "latitude": 19.704,
+    "longitude": 109.207
+  },
+  "danzhou-eman": {
+    "name": "峨蔓火山海岸（附近）",
+    "latitude": 19.842,
+    "longitude": 109.183
+  },
+  "lingao-jiefang": {
+    "name": "临高角解放公园海岸（附近）",
+    "latitude": 19.997,
+    "longitude": 109.714
+  },
+  "lingao-golden": {
+    "name": "金沙滩海滨岸段（附近）",
+    "latitude": 19.913,
+    "longitude": 109.689
+  },
+  "chengmai-yingbin": {
+    "name": "盈滨半岛海滩（附近）",
+    "latitude": 19.945,
+    "longitude": 110.033
+  },
+  "chengmai-yubao": {
+    "name": "玉包港海滨岸段（附近）",
+    "latitude": 19.891,
+    "longitude": 109.951
+  }
+}
